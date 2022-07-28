c504c3945bc352a5ee59bd1dcc70023b9627bbd4
Subhajit <subhabera211@gmail.com>
2022-07-28 15:56:48 +0530

    adding post and compose tab

--- app.js
+++ app.js
@@ -35,8 +35,36 @@ app.get("/", function (req, res) {
     });
 });
 
+app.get("/compose", function (req, res) {
+    res.render("compose");
+});
+
+app.post("/compose", function (req, res) {
+    const post = new Post({
+        title: req.body.postTitle,
+        content: req.body.postBody
+    });
 
 
+    post.save(function (err) {
+        if (!err) {
+            res.redirect("/");
+        }
+    });
+});
+
+app.get("/posts/:postId", function (req, res) {
+
+    const requestedPostId = req.params.postId;
+
+    Post.findOne({ _id: requestedPostId }, function (err, post) {
+        res.render("post", {
+            title: post.title,
+            content: post.content
+        });
+    });
+
+});
 
 
 app.get("/about", function (req, res) {
